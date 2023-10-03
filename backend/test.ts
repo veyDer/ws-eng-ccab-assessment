2f5758c1dc25c7b4f142d8d25eab67a68dd6606a
Bart Rudzki <bart.rudzki@globe.com.ph>
2023-10-03 13:09:54 +1300

    debug: test to illustrate the issue

--- backend/test.ts
+++ backend/test.ts
@@ -1,6 +1,6 @@
 import { performance } from "perf_hooks";
 import supertest from "supertest";
-import { buildApp } from "./app";
+import { ChargeResult, buildApp } from "./app";
 
 const app = supertest(buildApp());
 
@@ -15,8 +15,37 @@ async function basicLatencyTest() {
     console.log(`Latency: ${performance.now() - start} ms`);
 }
 
+const sleep = (delay: number) => new Promise((resolve => setTimeout(resolve, delay)));
+async function basicChargeTest() {
+    const promises: Promise<supertest.Response>[] = [];
+    const results: ChargeResult[] = [];
+
+    await app.post("/reset").expect(204);
+    for (let i=0; i<22; i++) {
+        const delay = 1000*Math.floor(i/2);
+        const promise = sleep(delay).then(
+            () => app
+                .post("/charge")
+                .expect(200)
+                .then((response) => {
+                    const result: ChargeResult = {
+                        isAuthorized: response.body.isAuthorized,
+                        remainingBalance: response.body.remainingBalance,
+                        charges: response.body.charges,
+                    };
+                    results.push(result);
+                    return response;
+                })
+            );
+        promises.push(promise);
+    }
+    await Promise.allSettled(promises);
+    console.log(`Results: ${JSON.stringify(results, undefined, 2)}`);    
+}
+
 async function runTests() {
-    await basicLatencyTest();
+    // await basicLatencyTest();
+    await basicChargeTest();
 }
 
 runTests().catch(console.error);
